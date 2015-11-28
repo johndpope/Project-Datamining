82a4035d52fe23a45722a115070d35540553ee1f
rkmalik <rkmalik@ufl.edu>
2015-11-28 11:26:04 -0500

    Implemented Probablity based CosineSimilarity Algorithm

--- finalproject/src/topicmodelling/FrequencyBasedCosineSimiarity.java
+++ finalproject/src/topicmodelling/FrequencyBasedCosineSimiarity.java
@@ -16,7 +16,7 @@ import javax.swing.plaf.synth.SynthSeparatorUI;
 public class FrequencyBasedCosineSimiarity {
 
 	public static final String genre[] = { "adventure", "comedy", "crime", "drama", "family", "fantasy", "horror",
-			"musical", "mystery", "romance", "thriller", "war"};
+			"musical", "mystery", "romance", "thriller", "war" };
 
 	public class VectorFrequencies {
 		int freqVector1;
@@ -81,45 +81,49 @@ public class FrequencyBasedCosineSimiarity {
 		double VectAB = 0.0000000;
 		double VectA_Sq = 0.0000000;
 		double VectB_Sq = 0.0000000;
-		
-		/*StringBuilder output = new StringBuilder ("");
-		try {
-			
-			
-			FileWriter filewriter = new FileWriter(new File("/home/rkmalik/Downloads/vectorOutput/finaloutput/finaloutput.txt"));
-			BufferedWriter writer = new BufferedWriter(filewriter);*/
-			
-			for (int i = 0; i < distincwordslist.size(); i++) {
-				VectorFrequencies vals12 = wordFrequenciesVector.get(distincwordslist.get(i));
 
-				double freq1 = (double) vals12.freqVector1;
-				double freq2 = (double) vals12.freqVector2;
-				
-				//output.append(String.format("%-20s  #%-10.1f   #%-10.1f\n", distincwordslist.get(i), freq1, freq2));
-				
-				//System.out.printf("%-20s  #%-10.1f   #%-10.1f\n", distincwordslist.get(i), freq1, freq2);
-				// System.out.println(Distinct_words_text_1_2.get(i) + " #" + freq1
-				// + " #" + freq2);
+		/*
+		 * StringBuilder output = new StringBuilder (""); try {
+		 * 
+		 * 
+		 * FileWriter filewriter = new FileWriter(new File(
+		 * "/home/rkmalik/Downloads/vectorOutput/finaloutput/finaloutput.txt"));
+		 * BufferedWriter writer = new BufferedWriter(filewriter);
+		 */
 
-				VectAB = VectAB + (freq1 * freq2);
+		for (int i = 0; i < distincwordslist.size(); i++) {
+			VectorFrequencies vals12 = wordFrequenciesVector.get(distincwordslist.get(i));
 
-				VectA_Sq = VectA_Sq + freq1 * freq1;
-				VectB_Sq = VectB_Sq + freq2 * freq2;
-			}
-			
-			output.append(String.format("VectAB " + VectAB + " VectA_Sq " + VectA_Sq + " VectB_Sq " + VectB_Sq));
-			
-			
-			//System.out.println("VectAB " + VectAB + " VectA_Sq " + VectA_Sq + " VectB_Sq " + VectB_Sq);
-			sim_score = ((VectAB) / (Math.sqrt(VectA_Sq) * Math.sqrt(VectB_Sq)));
-			/*writer.write(output.toString());
-			filewriter.close();
-			
-		} catch (IOException e) {
-			
-			e.printStackTrace();
-		}*/
-		
+			double freq1 = (double) vals12.freqVector1;
+			double freq2 = (double) vals12.freqVector2;
+
+			// output.append(String.format("%-20s #%-10.1f #%-10.1f\n",
+			// distincwordslist.get(i), freq1, freq2));
+
+			// System.out.printf("%-20s #%-10.1f #%-10.1f\n",
+			// distincwordslist.get(i), freq1, freq2);
+			// System.out.println(Distinct_words_text_1_2.get(i) + " #" + freq1
+			// + " #" + freq2);
+
+			VectAB = VectAB + (freq1 * freq2);
+
+			VectA_Sq = VectA_Sq + freq1 * freq1;
+			VectB_Sq = VectB_Sq + freq2 * freq2;
+		}
+
+		output.append(String.format("VectAB " + VectAB + " VectA_Sq " + VectA_Sq + " VectB_Sq " + VectB_Sq));
+
+		// System.out.println("VectAB " + VectAB + " VectA_Sq " + VectA_Sq + "
+		// VectB_Sq " + VectB_Sq);
+		sim_score = ((VectAB) / (Math.sqrt(VectA_Sq) * Math.sqrt(VectB_Sq)));
+		/*
+		 * writer.write(output.toString()); filewriter.close();
+		 * 
+		 * } catch (IOException e) {
+		 * 
+		 * e.printStackTrace(); }
+		 */
+
 		return (sim_score);
 	}
 
@@ -127,17 +131,15 @@ public class FrequencyBasedCosineSimiarity {
 		FrequencyBasedCosineSimiarity cs1 = new FrequencyBasedCosineSimiarity();
 
 		String path = "/home/rkmalik/Downloads/vectorOutput/finaldata/";
-		HashMap<String, String> allgenre = new HashMap<String, String> ();
+		HashMap<String, String> allgenre = new HashMap<String, String>();
 
 		// This will reference one line at a time
 		String line = null;
-		
-		
 
 		for (int i = 0; i < genre.length; i++) {
-			
+
 			String fileName = path + "/" + genre[i];
-			
+
 			try {
 				FileReader fileReader = null;
 				// FileReader reads text files in the default encoding.
@@ -150,7 +152,7 @@ public class FrequencyBasedCosineSimiarity {
 				while ((buf = bufferedReader.readLine()) != null) {
 					buffer += buf;
 				}
-				
+
 				allgenre.put(genre[i], buffer);
 				System.out.println("-----");
 				System.out.println(buffer);
@@ -160,118 +162,77 @@ public class FrequencyBasedCosineSimiarity {
 				e.printStackTrace();
 			}
 		}
-		
-		//fileName +=
-		
-		StringBuilder testdata = new StringBuilder ("");
-		
+
+		// fileName +=
+
+		StringBuilder testdata = new StringBuilder("");
+
 		try {
 			FileReader fileReader = null;
 			// FileReader reads text files in the default encoding.
-			//fileReader = new FileReader("/home/rkmalik/Downloads/vectorOutput/test/500DaysofSummer.txt");
-			//fileReader = new FileReader("/home/rkmalik/Downloads/vectorOutput/test/dracula.txt");
+			// fileReader = new
+			// FileReader("/home/rkmalik/Downloads/vectorOutput/test/500DaysofSummer.txt");
+			// fileReader = new
+			// FileReader("/home/rkmalik/Downloads/vectorOutput/test/dracula.txt");
 			fileReader = new FileReader("/home/rkmalik/Downloads/vectorOutput/test/thePianist.txt");
-			
+
 			// Always wrap FileReader in BufferedReader.
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
-			//String buffer = "";
+			// String buffer = "";
 			String buf = "";
 
 			while ((buf = bufferedReader.readLine()) != null) {
 				testdata.append(buf);
-				//testdata += buf;
+				// testdata += buf;
 			}
-			
-			//allgenre.put(genre[i], buffer);
-			//System.out.println("-----");
-			//System.out.println(buffer);
+
+			// allgenre.put(genre[i], buffer);
+			// System.out.println("-----");
+			// System.out.println(buffer);
 			fileReader.close();
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		
-		/*System.out.println("[Word # VectorA # VectorB]");
-		String bow_romance = new String(
-				"baby bedroom breath card chest child coffee clothes kiss expression red pretty love wife tight tonight");
-		String bow_scifi = new String(
-				"alien apes lightsaber spider anakin signal predators nasa laser launch obi spacecraft information pod jedi stars robot raptor solar cables human");
-		String bow_war = new String(
-				"armed alive attack bows broken castle burning debris explodes grenade guns horses sword mission refugees samurai soldiers");
-		String bow_horror = new String(
-				"dripping buried tearing corpse killers knives murdered vine scissors zombies burned needle tearing survivors limbs werewolf");
-		String bow_comedy = new String(
-				"laughter enjoy applause aladdin funny rolling embarrassed glad joke stupid chicken weird yells");
-		String bow_crime = new String(
-				"agent aims badge jacket footsteps steel involved victims suspect witness security fbi costume cell burn");
-
-		LinkedList<String> bagofwords = new LinkedList<String>();
-		bagofwords.add(bow_romance);
-		bagofwords.add(bow_scifi);
-		bagofwords.add(bow_war);
-		bagofwords.add(bow_horror);
-		bagofwords.add(bow_comedy);
-		bagofwords.add(bow_crime);
-
-		LinkedList<String> genre = new LinkedList<String>();
-		genre.add("romance");
-		genre.add("scifi");
-		genre.add("war");
-		genre.add("horror");
-		genre.add("comedy");
-		genre.add("crime");*/
-
-
-		
 		System.out.println("\n\n");
-		
-		
-		
-		
+
 		try {
-			
-			
-			FileWriter filewriter = new FileWriter(new File("/home/rkmalik/Downloads/vectorOutput/finaloutput/finaloutput.txt"));
+
+			FileWriter filewriter = new FileWriter(
+					new File("/home/rkmalik/Downloads/vectorOutput/finaloutput/finaloutput.txt"));
 			BufferedWriter writer = new BufferedWriter(filewriter);
-			
-			
+
 			for (int i = 0; i < allgenre.size(); i++) {
-				
-				StringBuilder output = new StringBuilder ("");
-				
+
+				StringBuilder output = new StringBuilder("");
+
 				output.append(String.format("%30s\n", "Processing " + genre[i]));
 
 				System.out.printf("%30s\n", "Processing " + genre[i]);
-				
-				//System.out.println(allgenre.get(genre[i]));
-				/*double sim_score = cs1.cosineSimilarityScore(
-						"time line alright that's sitting stick call governor minister alex treatment v.o long alex's fumbling lips ext draw family reply what's regard play imaginable boy victims responsibility crast eggs deeply heavy miserable ghastly half shut drops tradition exit chateau started tea magic horrible rest spaghetti straightens lucky nurse rubinstein slooshy speaking interest fortnight's couldn't salary laughing concentration poisoned harm familiar takes peacock meet tracks lovely waiting remembered today bible cartoon badly snuffed move tolchock slides prisoner nest mum inadvertently arch munching officer mad secret we'll legs i'd recorder parents offence jolly medicine beethoven woke belongs police complaints year's welly troubling beak aides owww questions realising practised rare years sick opposed encounter wrapped full cages hearts voice find here's possibly drinkers vitamins holding notebook accept side obsequious shoe sense wearing suppose chosen dimmed youth socially pulls block involved huh feet trolley putting facing beating meal life you'd carrying afternoon lets providence watch pops white phone trailed reaction parcel twitches fed alexander's girl begins understanding fell escort inserts ready noticed didn't christ bursts headlines fine pushed corridor light grave world spare battle ludwig film eating breaking visual rubbed reporters cries leans carving moans days you'll audible vino bandages buttoning snuff slowly concerned problem show cup verdict wore party punished book safe lies damned mend horrorshow lesson device called beg too.very surprised chest popularity",
-						allgenre.get(genre[i]));*/
+
+				// System.out.println(allgenre.get(genre[i]));
+				/*
+				 * double sim_score = cs1.cosineSimilarityScore(
+				 * "time line alright that's sitting stick call governor minister alex treatment v.o long alex's fumbling lips ext draw family reply what's regard play imaginable boy victims responsibility crast eggs deeply heavy miserable ghastly half shut drops tradition exit chateau started tea magic horrible rest spaghetti straightens lucky nurse rubinstein slooshy speaking interest fortnight's couldn't salary laughing concentration poisoned harm familiar takes peacock meet tracks lovely waiting remembered today bible cartoon badly snuffed move tolchock slides prisoner nest mum inadvertently arch munching officer mad secret we'll legs i'd recorder parents offence jolly medicine beethoven woke belongs police complaints year's welly troubling beak aides owww questions realising practised rare years sick opposed encounter wrapped full cages hearts voice find here's possibly drinkers vitamins holding notebook accept side obsequious shoe sense wearing suppose chosen dimmed youth socially pulls block involved huh feet trolley putting facing beating meal life you'd carrying afternoon lets providence watch pops white phone trailed reaction parcel twitches fed alexander's girl begins understanding fell escort inserts ready noticed didn't christ bursts headlines fine pushed corridor light grave world spare battle ludwig film eating breaking visual rubbed reporters cries leans carving moans days you'll audible vino bandages buttoning snuff slowly concerned problem show cup verdict wore party punished book safe lies damned mend horrorshow lesson device called beg too.very surprised chest popularity"
+				 * , allgenre.get(genre[i]));
+				 */
 				double sim_score = cs1.cosineSimilarityScore(testdata.toString(), allgenre.get(genre[i]), output);
 				System.out.println("Cosine similarity score for " + genre[i] + "= " + sim_score);
 				System.out.println("\n\n");
-				
+
 				output.append("\n\nCosine similarity score for " + genre[i] + "= " + sim_score + "\n\n");
-				
-				 writer.write(output.toString());
-				
+
+				writer.write(output.toString());
+
 			}
-			
-			
-			
-			
-			
-			
-				filewriter.close();
-			
+
+			filewriter.close();
+
 		} catch (IOException e) {
-			
+
 			e.printStackTrace();
 		}
-		
-		
-		 
 
 	}
 }
---
+++ finalproject/src/topicmodelling/ProbablityBasedCosineSimilarity.java
@@ -0,0 +1,329 @@
+package topicmodelling;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.Map;
+
+//calculates the cosine similarity between two texts / documents etc., (having each word separated by space)
+public class ProbablityBasedCosineSimilarity {
+
+	public static final String genre[] = { "adventure", "comedy", "crime", "drama", "family", "fantasy", "horror",
+			"musical", "mystery", "romance", "thriller", "war" };
+	private static final HashMap<String, HashMap<String, Double>> trainGenreProbablities = new HashMap<String, HashMap<String, Double>>();
+	private static final HashMap<String, HashMap<String, Double>> testDataProbablities = new HashMap<String, HashMap<String, Double>>();
+	private static StringBuilder output = null;
+	
+	public class VectorProbablities {
+		double trainVectorProbablity;
+		double testVectorProbablity;
+
+		VectorProbablities(double trainVectorProb, double testVectorProb) {
+			this.trainVectorProbablity = trainVectorProb;
+			this.testVectorProbablity = testVectorProb;
+		}
+
+		public void updateValues(double trainVectorProb, double testVectorProb) {
+			this.trainVectorProbablity = trainVectorProb;
+			this.testVectorProbablity = testVectorProb;
+		}
+	}
+
+	public double cosineSimilarityScore(HashMap<String, Double> trainDocument, HashMap<String, Double> testDocument) {
+		double sim_score = 0.0000000;
+		// 1. Identify distinct words from both documents
+		Hashtable<String, VectorProbablities> wordProbablityVector = new Hashtable<String, ProbablityBasedCosineSimilarity.VectorProbablities>();
+		LinkedList<String> distincwordslist = new LinkedList<String>();
+
+		// Prepare the word Probability vector for train data
+		for (Map.Entry<String, Double> trainDocEntry : trainDocument.entrySet()) {
+			
+			String curWord = trainDocEntry.getKey();
+			Double curProb = trainDocEntry.getValue();
+			
+			if (wordProbablityVector.containsKey(curWord)) {
+				VectorProbablities vals1 = wordProbablityVector.get(curWord);
+				if (curProb > vals1.trainVectorProbablity) {
+					vals1.trainVectorProbablity = curProb;
+					wordProbablityVector.put(curWord, vals1);
+				}
+			} else {
+				
+				VectorProbablities vals1 = new VectorProbablities(curProb, 0.0);
+				wordProbablityVector.put(curWord, vals1);
+				distincwordslist.add(curWord);
+			}
+			
+		}
+		
+		// Prepare the word Probability vector for train data
+		for (Map.Entry<String, Double> testDocEntry : testDocument.entrySet()) {
+			
+			String curWord = testDocEntry.getKey();
+			Double curProb = testDocEntry.getValue();
+			
+			if (wordProbablityVector.containsKey(curWord)) {
+				VectorProbablities vals1 = wordProbablityVector.get(curWord);
+				if (curProb > vals1.testVectorProbablity) {
+					vals1.testVectorProbablity = curProb;
+					wordProbablityVector.put(curWord, vals1);
+				}
+			} else {
+				
+				VectorProbablities vals1 = new VectorProbablities(0.0, curProb);
+				wordProbablityVector.put(curWord, vals1);
+				distincwordslist.add(curWord);
+			}
+			
+		}
+
+		// calculate the cosine similarity score.
+		double VectAB = 0.0000000;
+		double VectA_Sq = 0.0000000;
+		double VectB_Sq = 0.0000000;
+
+		for (int i = 0; i < distincwordslist.size(); i++) {
+			VectorProbablities vals12 = wordProbablityVector.get(distincwordslist.get(i));
+
+			double freq1 = (double) vals12.trainVectorProbablity;
+			double freq2 = (double) vals12.testVectorProbablity;
+
+			VectAB = VectAB + (freq1 * freq2);
+
+			VectA_Sq = VectA_Sq + freq1 * freq1;
+			VectB_Sq = VectB_Sq + freq2 * freq2;
+		}
+
+		
+		System.out.println(distincwordslist);
+		
+		
+		output.append(String.format("VectAB " + VectAB + " VectA_Sq " + VectA_Sq + " VectB_Sq " + VectB_Sq));
+		sim_score = ((VectAB) / (Math.sqrt(VectA_Sq) * Math.sqrt(VectB_Sq)));
+		return sim_score;
+	}
+
+	private static void parseTrainDataLine(String line, HashMap<String, Double> probablityList) {
+		
+		if (line.trim().isEmpty())
+			return;
+		
+		String[] tabbasedtokens = line.split("\t");
+		String token = null;
+		String probablity = null;
+
+		if (tabbasedtokens.length == 2) {
+
+			token = tabbasedtokens[0].trim();
+			// Check if token is already present in the map then get the
+			// Probability.
+
+			Double probablityVal = probablityList.get(token);
+
+			// If token is not already present then put this token in the hashmap
+			if (probablityVal == null) {
+				probablityVal = Double.parseDouble(tabbasedtokens[1]);
+				probablityList.put(token, probablityVal);
+
+				// Else find the one with maximum probability
+			} else {
+				Double curProbVal = Double.parseDouble(tabbasedtokens[1]);
+
+				if (curProbVal > probablityVal) {
+					probablityList.put(token, curProbVal);
+				}
+			}
+		} else {
+			throw new IllegalArgumentException("Illegal Format");
+		}
+	}
+	
+	private static void parseTestDataLine(String line, HashMap<String, Double> curTestDataProbablities) {
+		if (line.trim().isEmpty())
+			return;
+		
+		String[] tabbasedtokens = line.split("\t");
+		String[] spacebasedtokens = null;
+		String probablity = null;
+
+		if (tabbasedtokens.length == 3) {
+
+			spacebasedtokens = tabbasedtokens[2].trim().split(" ");
+			// Check if token is already present in the map then get the
+			// Probability.
+			
+			if (spacebasedtokens.length>=2) {
+				
+				// First find the the probability token  
+				Double curProbablity = Double.parseDouble(tabbasedtokens[1].trim());
+				
+				for (int i = 1; i < spacebasedtokens.length; i++) {
+					String token = spacebasedtokens[i].trim();
+					Double probablityVal = curTestDataProbablities.get(token);
+					
+					// If token is not already present then put this token in the hashmap
+					if (probablityVal == null) {
+						curTestDataProbablities.put(token, curProbablity);
+						// Else find the one with maximum probability
+					} else {
+						if (curProbablity > probablityVal) {
+							curTestDataProbablities.put(token, curProbablity);
+						}
+					}
+				}
+			} else {
+				throw new IllegalArgumentException("Invalid test data format.");
+			}
+		} else {
+			throw new IllegalArgumentException("Invalid test data format.");
+		}
+	}
+	
+	public static void main(String[] args) {
+		ProbablityBasedCosineSimilarity cs1 = new ProbablityBasedCosineSimilarity();
+
+		// Find the path of the parent Directory so that we can point to the
+		// testdata and traindata
+		Path path = null;
+		String parentPath = null;
+
+		try {
+			path = Paths.get(ProbablityBasedCosineSimilarity.class.getResource(".").toURI());
+			parentPath = path.getParent().getParent().toString();
+
+		} catch (URISyntaxException e) {
+			System.out.println("Syntex not correct.");
+		}
+
+		String testdata = parentPath.toString() + "/testdata/";
+		String traindata = parentPath.toString() + "/traindata/";
+		String cosinesummary = parentPath.toString() + "/cosinesimilaritysummary/";
+
+		
+		/*********************Process Train Data*******************/
+		
+		// This will reference one line at a time
+		String line = null;
+
+		for (int i = 0; i < genre.length; i++) {
+
+			String fileName = traindata + genre[i];
+
+			try {
+				FileReader fileReader = null;
+				// FileReader reads text files in the default encoding.
+				fileReader = new FileReader(fileName);
+				// Always wrap FileReader in BufferedReader.
+				BufferedReader bufferedReader = new BufferedReader(fileReader);
+				String buffer = "";
+				String buf = "";
+
+				HashMap<String, Double> genreProbablity = new HashMap<String, Double>();
+
+				while ((buf = bufferedReader.readLine()) != null) {
+
+					try {
+						parseTrainDataLine(buf, genreProbablity);
+					} catch (IllegalArgumentException e) {
+						e.printStackTrace();
+					}
+				}
+
+				trainGenreProbablities.put(genre[i], genreProbablity);
+				fileReader.close();
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		/*********************Train Data Processed*******************/
+		
+		/*********************Process Test Data**********************/
+		
+		
+		
+		File dir = new File (testdata);
+		File [] fileList = dir.listFiles();
+		
+		if (fileList!=null) {
+
+			for (File file : fileList) {
+				HashMap<String, Double> curTestDataProbablities = new HashMap<String, Double>(); 
+				try {
+					
+					FileReader fileReader = new FileReader(file);
+					// Always wrap FileReader in BufferedReader.
+					BufferedReader bufferedReader = new BufferedReader(fileReader);
+					String buf = "";
+
+					while ((buf = bufferedReader.readLine()) != null) {
+						parseTestDataLine(buf, curTestDataProbablities);
+					}
+					fileReader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				testDataProbablities.put(file.getName(), curTestDataProbablities);
+			}
+			
+		} else {
+			System.out.println("No test data files available");
+			return;
+		}
+		
+
+		/*********************Test Data Processed*******************/
+		
+		System.out.println("\n\n");
+		
+		/**
+		 * Algorithm Steps:
+		 * 	1. Read each processed data in the TestBuffer
+		 *  2. Find the cosine similarity against each of the Genre train data
+		 *  3. Prepare a summary file for each of the Test data.
+		 * */
+		
+		try {
+			for (Map.Entry<String, HashMap<String, Double>> testDataEntry : testDataProbablities.entrySet()) {
+				
+				String summaryFileName = cosinesummary+"summary_"+testDataEntry.getKey();
+				FileWriter filewriter = new FileWriter(summaryFileName);
+				BufferedWriter writer = new BufferedWriter(filewriter);
+				
+				
+				for (Map.Entry<String, HashMap<String, Double>> trainDataEntry : trainGenreProbablities.entrySet()) {
+					
+					String genre = trainDataEntry.getKey();
+					
+					output = new StringBuilder("");
+					output.append(String.format("%30s\n", "Processing " + genre));
+					System.out.printf("%30s\n", "Processing " + genre);
+					
+					double sim_score = cs1.cosineSimilarityScore(trainDataEntry.getValue(), testDataEntry.getValue());
+					System.out.println("Cosine similarity score for " + genre + "= " + sim_score);
+					System.out.println("\n\n");
+
+					output.append("\n\nCosine similarity score for " + genre + "= " + sim_score + "\n\n");
+
+					writer.write(output.toString());
+					
+				}
+				writer.close();
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
